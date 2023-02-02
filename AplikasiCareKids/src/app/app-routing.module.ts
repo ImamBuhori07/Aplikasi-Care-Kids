03af9a4e909b72295c62b95d05f42959ad8e92b6
adeharyantonailunnabhan <adeharyantonailunnabhan@gmail.com>
2023-02-02 12:08:27 +0700

    menambahakan navbar user

--- AplikasiCareKids/src/app/app-routing.module.ts
+++ AplikasiCareKids/src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-//pages
-import { BerandaComponent } from './user/beranda/beranda.component';
-import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   // Default route
