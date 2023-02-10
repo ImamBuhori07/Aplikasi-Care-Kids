80e399626ed1f8bb2106d2c3a8112a7f325af6bc
ImamBuhori07 <imambuhoritheaa@gmail.com>
2023-02-10 21:07:55 +0700

    Menghapus code tambahpostingan

--- AplikasiCareKids/src/app/admin/tambahpostingan/tambahpostingan.component.ts
+++ AplikasiCareKids/src/app/admin/tambahpostingan/tambahpostingan.component.ts
@@ -8,32 +8,7 @@ import { AdminService } from '../admin.service';
   templateUrl: './tambahpostingan.component.html',
   styleUrls: ['./tambahpostingan.component.css']
 })
-export class TambahpostinganComponent implements OnInit{
+export class TambahpostinganComponent {
 
-  constructor (
-    private _Adminservice : AdminService
-  ){}  
-  
-  ngOnInit(): void {
-      this.getCategory()
-  }
-
-  getCategory(){
-    this._Adminservice.category().subscribe(response: => {
-      this.getCategory = response ["category"]
-      console.log(this.getCategory)
-    })
-  }
-
-    deletecategory(name_category : string){
-      this._Adminservice.deletecategory(name_category).subscribe(response => {
-        if(Response.status == 200){
-          alert("Berhasil Hapus Category")
-          location.reload()
-        }else {
-          alert("Gagal menghapus Category")
-        }
-      })
-    }
   }
 
