120276f9c81f32751c1725929f5d3a89d6884194
ImamBuhori07 <imambuhoritheaa@gmail.com>
2023-02-21 19:25:18 +0700

    yes

--- AplikasiCareKids/src/app/admin/tambahpostingan/tambahpostingan.component.ts
+++ AplikasiCareKids/src/app/admin/tambahpostingan/tambahpostingan.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { response } from 'express';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Admin,article,Category } from '../admin.model';
 import { AdminService } from '../admin.service';
 
@@ -11,14 +12,38 @@ import { AdminService } from '../admin.service';
 
    
 
-export class TambahpostinganComponent {
+export class TambahpostinganComponent implements OnInit {
    
+    formarticle : FormGroup
+    category : Category [] = []
+    articleId : number =1
+
+    constructor(
+      private _AdminService : AdminService,
+      private _FormBuilder : FormBuilder,
+      private _router : Router,
+      private _Activatedroute : ActivatedRoute,
+      ){
+
+        this.formarticle = _FormBuilder.group ({
+          'articleId' : new FormControl (null),
+          'title' : new FormControl (null),
+          'description' : new FormControl (null),
+          'content' : new FormControl (null),
+          'category_id' : new FormControl (null),
+          'status_id' : new FormControl (null),
+          'thumbnail' : new FormControl (null),
+          'video' : new FormControl (null),
+        })
 
 
-    judul : string ="Tambah Postingan";
+      }
 
-    constructor(private AdminService : AdminService){}
-
-   
+  ngOnInit(): void {
+      
   }
+
+  }
+
+  
 
