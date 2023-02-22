dcb3ceee7c5836664ed126f09dfb640470a3560a
adeharyantonailunnabhan <adeharyantonailunnabhan@gmail.com>
2023-02-22 18:29:03 +0700

    integrasi dashboard admin

--- AplikasiCareKids/src/app/admin/admin.service.ts
+++ AplikasiCareKids/src/app/admin/admin.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {  article, Category, savearticle, saveCategory, savestatusarticle, statusarticle } from './admin.model';
+import { article, Category, savearticle, saveCategory, savestatusarticle, statusarticle } from './admin.model';
 import { environment } from 'src/environments/environment';
 
 
@@ -10,19 +10,51 @@ import { environment } from 'src/environments/environment';
 })
 export class AdminService {
 
+  //url
+  profile = 'http://127.0.0.1:8000/api/profile';
+  visitor = 'http://127.0.0.1:8000/api/visitor';
+  comment = 'http://127.0.0.1:8000/api/comment';
+  pesan = 'http://localhost:8000/api/contact';
 
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {
+    this.getVisitor();
+    this.getAllComment();
+    this.getPesan();
+  }
 
   listArticle() {
     return this.http.get<article[]>(`${environment.baseUrl}/article`)
   }
 
+  //dashboard
+  getProfileById(id: number) {
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem('token'));
+    return this.http.get(this.profile + '/' + id, { headers: headers })
+  }
+
+  getVisitor() {
+    return this.http.get(this.visitor);
+  }
+
+  getAllComment() {
+    return this.http.get(this.comment);
+  }
+
+  getPesan() {
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', 'Bearer ' + localStorage.getItem('token'));
+    return this.http.get(this.pesan, { headers: headers });
+  }
+
+
   savearticle(article: savearticle) {
     return this.http.post(`${environment.baseUrl}/article`, article, { observe: "response" })
   }
 
-  Updatearticle(article: savearticle){
-    return this.http.put(`${environment.baseUrl}/article`, article, {observe : "response"})
+  Updatearticle(article: savearticle) {
+    return this.http.put(`${environment.baseUrl}/article`, article, { observe: "response" })
   }
 
   FindarticleById(articleId: number) {
--- AplikasiCareKids/src/app/admin/dashboard/dashboard.component.ts
+++ AplikasiCareKids/src/app/admin/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AdminService } from '../admin.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,4 +9,46 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  profile: any;
+  visitor: any;
+  comment: any;
+  pesan: any;
+  totalcomment: number = 0;
+  totalpesan: number = 0;
+
+  constructor(private adminService: AdminService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.getProfile();
+    this.getVisit();
+    this.getCommnet();
+    this.getPesan();
+  }
+
+  getProfile() {
+    let id = this.route.snapshot.paramMap.get('id');
+    1 && this.adminService.getProfileById(1).subscribe((res: any) => {
+      this.profile = Array.of(res.data);
+    });
+  }
+
+  getVisit() {
+    this.adminService.getVisitor().subscribe((res: any) => {
+      this.visitor = Array.of(res.data);
+    })
+  }
+
+  getCommnet() {
+    this.adminService.getAllComment().subscribe((res: any) => {
+      this.comment = res.data;
+      this.totalcomment = this.comment.length;
+    });
+  }
+
+  getPesan() {
+    this.adminService.getPesan().subscribe((res: any) => {
+      this.pesan = res.data;
+      this.totalpesan = this.pesan.length;
+    });
+  }
 }
